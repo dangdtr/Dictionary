8bb5f5eb6b1267cf5d60f7d6b92e49c46edbf5a0
nhat123-dev <minhnhat8dc@gmail.com>
2021-09-25 16:30:42 +0700

    Nhat updated

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,6 +2,6 @@ package Application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("up toan bo project len di");
+        System.out.println("ok duoc roi nhe");
     }
 }
