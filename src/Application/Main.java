1b7c613fe817f407276f7c6cecb88ce557bf6091
nhat123-dev <minhnhat8dc@gmail.com>
2021-09-25 15:06:30 +0700

    Nhat updated

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,6 +2,6 @@ package Application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lấy may 10 điểm BTL OOP");
+        System.out.println("up toan bo project len di");
     }
 }
