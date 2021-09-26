f3aedd4aa9fd7aa363616d93a5d5b35075a17070
nhat123-dev <minhnhat8dc@gmail.com>
2021-09-26 16:39:00 +0700

    update Main class

--- DictionaryVersion01/src/Main.java
+++ DictionaryVersion01/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Version 01 of Dictionary by java");
+        System.out.println("Oke clone xong roi nhe hihi!!");
     }
 }
