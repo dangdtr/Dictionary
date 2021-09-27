5216195ba3ecacf59fb44d2ef56df3c76e8ef348
g-nad <ddang.daodang@gmail.com>
2021-09-27 12:50:18 +0700

    Dang: updated v0.2 and fixed cmd ui

---
+++ DictionaryVersion0.2/src/Dictionary.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    /**
+     * declared bankWord is arrays of Word objects.
+     */
+    public ArrayList<Word> bankWord = new ArrayList<>();
+}
---
+++ DictionaryVersion0.2/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        System.out.println("Version 0.2 of Dictionary by java");
+        DictionaryCommandLine ver = new DictionaryCommandLine();
+
+        //optional: bao giờ test hoặc có GUI thì đóng cái này
+        int select = -1;
+        do {
+            System.out.println("-------------Dictionary Command Line--------------\n");
+            System.out.println("Các tùy chọn:");
+            System.out.println("0: Thoát chương trình");
+            System.out.println("1: Phiên bản commnline cơ bản (v0.1)");
+            System.out.println("2: Phiên bản commanline nâng cao (v0.2)");
+            System.out.print("Lựa chọn và ấn Enter để tiếp tục: ");
+            System.out.println();
+
+            Scanner scanner = new Scanner(System.in);
+            select = scanner.nextInt();
+
+            switch (select) {
+                case 0:
+                    System.out.println("Đang đóng chương chương...");
+                    Thread.sleep(500);
+                    System.out.println("Bye....:(");
+                    break;
+                case 1:
+                    ver.dictionaryBasic();
+                    break;
+                case 2:
+                    ver.dictionaryAdvanced();
+                    break;
+                default:
+                    break;
+            }
+        } while (select != 0);
+    }
+}
