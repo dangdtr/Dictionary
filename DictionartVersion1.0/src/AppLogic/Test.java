a1309e8bba4c84108f441350f25d9995961413e3
g-nad <ddang.daodang@gmail.com>
2021-10-05 21:39:11 +0700

    Dang: fixed & import data

--- DictionartVersion1.0/src/AppLogic/Test.java
+++ DictionartVersion1.0/src/AppLogic/Test.java
@@ -9,10 +9,10 @@ public class Test {
 
     private static DictionaryManagement dictionaryManagement = new DictionaryManagement();
     public static void main(String[] args){
-        dictionaryManagement.insertFromFile("./res/EV.txt");
+        dictionaryManagement.insertFromFile("./res/EVPart1.txt");
         Set<String> keySet = Dictionary.bankWord.keySet();
         for (String key : keySet) {
-            //System.out.println(key);
+            System.out.println(key);
         }
     }
 }
